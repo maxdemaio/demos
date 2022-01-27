d643fa839634f3b62ed072eab8a63d568c4f85bd
Maxwell DeMaio <maxdman321@gmail.com>
2022-01-27 12:16:40 -0500

    complete demo7 for constructor xml injection

---
+++ demo7-autowire-constructor-xml/src/main/java/com/maxdemaio/demo/Client.java
@@ -0,0 +1,14 @@
+package com.maxdemaio.demo;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        BlogPostService srv = (BlogPostService) context.getBean("blogPostService");
+        System.out.println("Done!");
+    }
+
+}
