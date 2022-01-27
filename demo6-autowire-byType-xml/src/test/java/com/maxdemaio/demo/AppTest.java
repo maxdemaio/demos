d7e1ebcae09dcab57f2f63d53dc23ae6fa20706a
Maxwell DeMaio <maxdman321@gmail.com>
2022-01-26 23:26:37 -0500

    add byName and byType autowire xml demos

---
+++ demo6-autowire-byType-xml/src/test/java/com/maxdemaio/demo/AppTest.java
@@ -0,0 +1,38 @@
+package com.maxdemaio.demo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :D
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
