a021958599e93d9a3c1e58157088407b5367b732
Maxwell DeMaio <maxdman321@gmail.com>
2022-05-06 12:42:11 -0400

    add tests for service getCustomer

--- data-demos/jpa-queries/src/test/java/com/maxdemaio/service/CustomerServiceTest.java
+++ data-demos/jpa-queries/src/test/java/com/maxdemaio/service/CustomerServiceTest.java
@@ -1,9 +1,11 @@
 package com.maxdemaio.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.maxdemaio.domain.Customer;
 import com.maxdemaio.dto.CustomerDto;
 import com.maxdemaio.exception.JpaQueriesException;
 import com.maxdemaio.repository.CustomerRepository;
+import org.json.JSONException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,14 +16,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.commons.lang3.StringUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import java.util.Optional;
 
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceTest {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Mock
     private CustomerRepository mockCustomerRepository;
@@ -35,25 +43,49 @@ public class CustomerServiceTest {
     }
 
     @Test
-    void test_insertCustomer() throws JpaQueriesException {
+    void test_insertCustomer() throws JpaQueriesException, JsonProcessingException, JSONException {
         // Given
-        CustomerDto testCustomer = new CustomerDto(7022713754L, "Test", 20, 'M', "Canton", 1);
+        CustomerDto testCustomerDto = new CustomerDto(7022713754L, "Test", 20, 'M', "Canton", 1);
 
         // When (void)
         when(mockCustomerRepository.saveAndFlush(any(Customer.class))).thenReturn(null);
 
         // Then
-        CustomerDto actual = customerService.insertCustomer(testCustomer);
-        assertEquals(testCustomer, actual);
+        // Compare JSON string values of POJOs
+        CustomerDto actual = customerService.insertCustomer(testCustomerDto);
+        JSONAssert.assertEquals(objectMapper.writeValueAsString(testCustomerDto), objectMapper.writeValueAsString(actual), JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    void test_getCustomer() throws JpaQueriesException, JsonProcessingException, JSONException {
+        // Given
+        Customer testCustomer = new Customer(7022713754L, "Test", 20, 'M', "Canton", 1);
+        CustomerDto testCustomerDto = new CustomerDto(7022713754L, "Test", 20, 'M', "Canton", 1);
+
+        // When
+        when(mockCustomerRepository.findById(any(Long.class))).thenReturn(Optional.of(testCustomer));
+
+        // Then
+        CustomerDto actual = customerService.getCustomer(testCustomerDto.getPhoneNumber());
+        JSONAssert.assertEquals(objectMapper.writeValueAsString(testCustomerDto), objectMapper.writeValueAsString(actual), JSONCompareMode.NON_EXTENSIBLE);
+    }
+
+    @Test
+    void test_getCustomer_failure_on_validate() throws JpaQueriesException, JsonProcessingException, JSONException {
+        // Given
+        Long invalidPhoneNo = 1000000000000000L;
+
+        // Then
+        assertThrows(JpaQueriesException.class, () -> customerService.getCustomer(invalidPhoneNo));
     }
 
     @Test
     void test_insertCustomer_failure_on_validate() throws JpaQueriesException {
         // Given
         String invalidName = StringUtils.repeat("x", 40);
-        CustomerDto testCustomer = new CustomerDto(7022713754L, invalidName, 20, 'M', "Canton", 1);
+        CustomerDto testCustomerDto = new CustomerDto(7022713754L, invalidName, 20, 'M', "Canton", 1);
 
         // Then
-        assertThrows(JpaQueriesException.class, () -> customerService.insertCustomer(testCustomer));
+        assertThrows(JpaQueriesException.class, () -> customerService.insertCustomer(testCustomerDto));
     }
 }
